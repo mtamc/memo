0108fc24f40d1cfdaad0f3ac62746a92247d6126
Tam CARRE <kokone@takat.su>
2022-01-15 18:37:20 +0100

    refactor

--- src/api/serverless_functions/films.js
+++ src/api/serverless_functions/films.js
@@ -1,9 +1,11 @@
 const { match } = require('ts-pattern')
 const responses = require('../utils/responses')
-const { segments, getUrlSegmentAndFind, getReqBodyAndCreate } = require('../utils/router')
+const { matchVerbAndNumberOfUrlSegment, getUrlSegmentAndFind, getReqBodyAndCreate } = require('../utils/router')
 
 exports.handler = async (event, context) =>
-  match([event.httpMethod, segments(event).length])
+  matchVerbAndNumberOfUrlSegment(event)
     .with(['GET', 1], () => getUrlSegmentAndFind('films', event))
     .with(['POST', 0], () => getReqBodyAndCreate('films', event))
     .otherwise(() => responses.badRequest())
+
+
