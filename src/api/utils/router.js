38cbf2062216a40c54d6242c602f0e45108857de
Your Name <your.email@example.com>
2022-01-16 10:27:07 +0100

    document backend and make it typesafe

--- src/api/utils/router.js
+++ src/api/utils/router.js
@@ -1,42 +1,66 @@
+/**
+ * @file This file exports the utility functions that Netlify
+ * functions should be using for maximum simplicity.
+ *
+ * Those functions help Netlify functions parse
+ * HTTP requests in the tersest way possible, perform DB
+ * queries and return HTTP responses.
+ */
+/** @typedef {import('@netlify/functions').HandlerEvent} Event */
+/** @typedef {import('./responses').Response} Response */
+/** @typedef {import('./errors').Error} Error */
 const { Result } = require('neverthrow')
 const { reqError } = require('./errors')
 const { create, findByRef } = require('./db')
 const responses = require('./responses')
 const { match } = require('ts-pattern')
 const { length } = require('ramda')
+const { tuple } = require('./general')
 
-const getUrlSegments = (event) =>
-  event.path
-    .replace(/\.netlify\/functions\/[^/]+/, '')
-    .replace(/api\/[^/]+/, '')
-    .split('/')
-    .filter((s) => s)
+/**
+ * This is the main routing utility we use in our Netlify functions.
+ * @param {Event} event
+ * @example
+ *     matchVerbAndNumberOfUrlSegments(event)
+ *      .with(['GET', 1], () => getFirstUrlSegmentAndFind('films', event))
+ *      .with(['POST', 0], () => getReqBodyAndCreate('films', event))
+ *      .otherwise(() => responses.badRequest())
+ */
+const matchVerbAndNumberOfUrlSegments = (event) =>
+  match(tuple([event.httpMethod, length(getUrlSegments(event))]))
 
-const routeArg = (event) => segments(event)[0]
-
+/** @type {(collection: string, event: Event) => Promise<Response> } */
 const getReqBodyAndCreate = (collection, event) =>
   getReqBody(event).match(
     (body) => create(collection, body),
     (err) => Promise.resolve(responses.badRequest(err)),
   )
 
-const getUrlSegmentAndFind = (collection, event) =>
-  findByRef(collection, routeArg(event))
-
-const matchVerbAndNumberOfUrlSegments = (event) =>
-  match([event.httpMethod, length(getUrlSegments(event))])
+/** @type {(collection: string, event: Event) => Promise<Response> } */
+const getFirstUrlSegmentAndFind = (collection, event) =>
+  findByRef(collection, getFirstSegment(event))
 
 module.exports = {
-  getUrlSegments,
-  routeArg,
   getReqBodyAndCreate,
-  getUrlSegmentAndFind,
-  matchVerbAndNumberOfUrlSegments
+  getUrlSegmentAndFind: getFirstUrlSegmentAndFind,
+  matchVerbAndNumberOfUrlSegments,
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
+/** @type {(event: Event) => Result<any, Error>} */
 const getReqBody = Result.fromThrowable(
   (event) => JSON.parse(event.body),
-  err => reqError(String(err)),
+  (err) => reqError(String(err)),
 )
+
+/** @type {(event: Event) => string} */
+const getFirstSegment = (event) => getUrlSegments(event)[0] ?? "<Shouldn't occur>"
+
+/** @type {(event: Event) => string[]} */
+const getUrlSegments = (event) =>
+  event.path
+    .replace(/\.netlify\/functions\/[^/]+/, '')
+    .replace(/api\/[^/]+/, '')
+    .split('/')
+    .filter((s) => s)
