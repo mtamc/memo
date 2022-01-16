fefa16974ff856dd34c5f7605e68ff0fce0c8b6e
Your Name <your.email@example.com>
2022-01-16 13:12:47 +0100

    validation for all data types

--- src/api/utils/parsers.js
+++ src/api/utils/parsers.js
@@ -18,17 +18,45 @@ const errors = require('./errors')
 
 /** @typedef {'films' | 'games'} ValidCollection */
 
+/** @type {<T>(parser: ZodType, x: T) => Result<T, Error>} */
+const validate = (parser, x) =>
+  Result.fromThrowable(parser.parse, (e) => errors.req(JSON.stringify(e)))(x)
+
+// Work (abstract) =================================================
+
+const entryTypeParser = z.enum(['Game', 'Film', 'TVShow', 'Book'])
+
+/**
+ * @typedef {object} Work
+ * @property {'Game'|'Film'|'TVShow'|'Book'} entryType
+ * @property {string} englishTranslatedTitle
+ * @property {string} [originalTitle]
+ * @property {number} [releaseYear]
+ * @property {number} [duration]
+ * @property {string} [imageUrl]
+ * @property {string[]} [genres]
+ */
+
+const workParser = z.object({
+  entryType: entryTypeParser,
+  englishTranslatedTitle: z.string(),
+  originalTitle: z.string().or(z.undefined()),
+  releaseYear: z.number().or(z.undefined()),
+  duration: z.number().or(z.undefined()),
+  imageUrl: z.string().or(z.undefined()),
+  genres: z.array(z.string()).or(z.undefined()),
+})
+
 // Films ===========================================================
 
 const filmParser = workParser.extend({
   entryType: z.literal('Film'),
-  staff: z.array(z.string()).or(z.undefined())
+  staff: z.array(z.string()).or(z.undefined()),
 })
 
 /**
  * @typedef {object} FilmProps
  * @property {'Film'} entryType
- * @property {string[]} [genres]
  * @property {string[]} [staff]
  *
  * @typedef {Work & FilmProps} Film
@@ -58,44 +86,50 @@ const gameParser = workParser.extend({
 /** @type Validator<Game> */
 const games = (x) => validate(gameParser, x)
 
+// TVShows =========================================================
+
+const tvShowParser = workParser.extend({
+  entryType: z.literal('TVShow'),
+  staff: z.array(z.string()).or(z.undefined()),
+  episodes: z.number().or(z.undefined()),
+})
+
+/**
+ * @typedef {object} TVShowProps
+ * @property {'TVShow'} entryType
+ * @property {string[]} [staff]
+ * @property {number} [episodes]
+ *
+ * @typedef {Work & TVShowProps} TVShow
+ */
+
+/** @type Validator<TVShow> */
+const tvShows = (x) => validate(tvShowParser, x)
+
+// Books ===========================================================
+
+const bookParser = workParser.extend({
+  entryType: z.literal('Book'),
+  authors: z.array(z.string()).or(z.undefined()),
+})
+
+/**
+ * @typedef {object} BookProps
+ * @property {'Book'} entryType
+ * @property {string[]} [authors]
+ *
+ * @typedef {Work & BookProps} Book
+ */
+
+/** @type Validator<Book> */
+const books = (x) => validate(bookParser, x)
+
 // Exports =========================================================
 
 /**@type {Object.<ValidCollection, Validator<any>>} */
 module.exports = {
   films,
   games,
+  tvShows,
+  books
 }
-
-///////////////////////////////////////////////////////////////////////////////
-
-
-/** @type {<T>(parser: ZodType, x: T) => Result<T, Error>} */
-var validate = (parser, x) =>
-  Result.fromThrowable(parser.parse, errors.req)(x)
-
-// Work ============================================================
-
-var entryTypeParser = z.enum(['Game', 'Film', 'TVShow', 'Book'])
-
-/**
- * @typedef {object} Work
- * @property {string} id
- * @property {'Game'|'Film'|'TVShow'|'Book'} entryType
- * @property {string} englishTranslatedTitle
- * @property {string} [originalTitle]
- * @property {number} [releaseYear]
- * @property {number} [duration]
- * @property {string} [imageUrl]
- * @property {string[]} [genres]
- */
-
-var workParser = z.object({
-  id: z.string(),
-  entryType: entryTypeParser,
-  englishTranslatedTitle: z.string(),
-  originalTitle: z.string().or(z.undefined()),
-  releaseYear: z.number().or(z.undefined()),
-  duration: z.number().or(z.undefined()),
-  imageUrl: z.string().or(z.undefined()),
-  genres: z.array(z.string()).or(z.undefined()),
-})
