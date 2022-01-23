56ac1ed4d30a1adf990c4c16a718780dfb28500d
Tam CARRE <kokone@takat.su>
2022-01-23 07:17:50 +0100

    more comments

--- src/frontend/_includes/js/components/index.js
+++ src/frontend/_includes/js/components/index.js
@@ -2,27 +2,26 @@
 const initComponent = ({ content, initializer, style }) => {
   let includeList = []
 
-  /**
-   * Include the component HTML inside another, but also remember to
-   * include that component's JS/initializer inside the parent's
-   * This function is provided as a parameter to `content`
-   */
+  // Include the component HTML inside another, but also remember to
+  // include that component's JS/initializer inside the parent's
+  // This function is provided as a parameter to `content`
   const include = (component) => {
     const unwrapped = access(component)
     includeList = [...includeList, unwrapped]
     return unwrapped.content
   }
 
-  /** Generate a identifier unique to that component's instance */
+  // Generate a identifier unique to that component's instance
+  // This is passed to `content`, `initializer` and `style`
   const id = uniqueId()
 
-  /** Create <style> tag if none exists */
+  // Create <style> tag if none exists
   const styleTag = $('head style')
   if (styleTag.length === 0) {
     $('head').prepend('<style></style>')
   }
 
-  /** Add the component style to the site if it has not already */
+  // Add the component style to the site if it has not already
   const componentStyle = style?.({ id })
   if (!styleTag.html().includes(componentStyle)) {
     styleTag.append(componentStyle)
@@ -31,8 +30,12 @@ const initComponent = ({ content, initializer, style }) => {
   return private({
     id,
     content: content({ id, include }),
+    // We create a new initializer that also contains the child initializers
     initializer: () => {
+      // This component's initializer, with the unique id passed into it
       initializer?.({ id })
+
+      // The child components' initializers
       includeList.forEach(({ initializer, id }) => {
         initializer?.({ id })
       })
@@ -40,9 +43,12 @@ const initComponent = ({ content, initializer, style }) => {
   })
 }
 
+/** Imperative way to replace an element inner HTML with a component */
 const setContent = (selector, component) => {
   const { content, initializer } = access(component)
+  // Set the inner HTML to the component's content
   $(selector).html(content)
+  // Run the component's initializer
   initializer?.()
 }
 
