46614664de187f602564539882df99247558b436
Your Name <your.email@example.com>
2022-01-29 20:21:33 +0100

    fix

--- src/frontend/_includes/js/utils/columns.js
+++ src/frontend/_includes/js/utils/columns.js
@@ -201,40 +201,40 @@ const playtimeFormatter = (durationInMin, row) => {
     : '-'
 }
 
-const relativeTime (ts) => {
-  const msPerMinute = 60 * 1000
-  const msPerHour = msPerMinute * 60
-  const msPerDay = msPerHour * 24
-  const msPerMonth = msPerDay * 30
-  const msPerYear = msPerDay * 365
+// const relativeTime (ts) => {
+  // const msPerMinute = 60 * 1000
+  // const msPerHour = msPerMinute * 60
+  // const msPerDay = msPerHour * 24
+  // const msPerMonth = msPerDay * 30
+  // const msPerYear = msPerDay * 365
 
-  const elapsed = Date.now() - ts
+  // const elapsed = Date.now() - ts
 
-  elapsed < msPerMinute ? round(elapsed/1000) + ' seconds ago' :
-  elapsed < msPerHour   ? round(elapsed/msPerMinute) + ' minutes ago' :
+  // elapsed < msPerMinute ? round(elapsed/1000) + ' seconds ago' :
+  // elapsed < msPerHour   ? round(elapsed/msPerMinute) + ' minutes ago' :
 
 
-  if (elapsed < msPerMinute) {
-       return Math.;
-  }
+  // if (elapsed < msPerMinute) {
+       // return Math.;
+  // }
 
-  else if ( {
-       return Math.;   
-  }
+  // else if ( {
+       // return Math.;   
+  // }
 
-  else if (elapsed < msPerDay ) {
-       return Math.round(elapsed/msPerHour ) + ' hours ago';   
-  }
+  // else if (elapsed < msPerDay ) {
+       // return Math.round(elapsed/msPerHour ) + ' hours ago';   
+  // }
 
-  else if (elapsed < msPerMonth) {
-      return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';   
-  }
+  // else if (elapsed < msPerMonth) {
+      // return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';   
+  // }
 
-  else if (elapsed < msPerYear) {
-      return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';   
-  }
+  // else if (elapsed < msPerYear) {
+      // return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';   
+  // }
 
-  else {
-      return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';   
-  }
-}
+  // else {
+      // return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';   
+  // }
+// }
