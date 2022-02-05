cfaf371dbf5a3cf85e7074af06c5b6721869d9ab
Your Name <your.email@example.com>
2022-02-05 09:09:36 +0100

    update fauna version && add script to fix commonMetadata duration values

--- src/db_maintenance/replace_api_durations_with_override_value.js
+++ src/db_maintenance/replace_api_durations_with_override_value.js
@@ -1,4 +1,8 @@
 #!/usr/bin/env node
+// some duration values in commonMetadata are broken due to a bug
+// (whilst the override value is correct)
+// so for now let's just replace every duration value in
+// commonMetadata with the override value if exists
 
 require('dotenv').config()
 const fs = require('fs')
@@ -12,25 +16,11 @@ const collections = [
   'bookEntries'
 ]
 
-const fixDate = (posix) => {
-  if (!posix) {
-    return undefined
-  }
-  const date = new Date(posix)
-  const year = date.getFullYear()
-  if (year > 4000) {
-    date.setFullYear(year - 2000)
-  } else if (year > 2030) {
-    date.setFullYear(year - 100)
-  }
-  return date.getTime()
-}
-
 ;(async () => {
   for (const collection of collections) {
     const entries = (await _findAllInCollection(collection)).data
     fs.writeFileSync(
-      `backup_${collection}_${new Date().toISOString()}.json`,
+      `../../../backup_${collection}_${new Date().toISOString()}.json`,
       JSON.stringify(entries)
     )
 
@@ -39,8 +29,10 @@ const fixDate = (posix) => {
       ...e,
       data: {
         ...e.data,
-        startedDate: fixDate(e.data.startedDate),
-        completedDate: fixDate(e.data.completedDate),
+        commonMetadata: {
+          ...e.data.commonMetadata,
+          duration: e.data.overrides?.duration || e.data.commonMetadata?.duration
+        }
       }
     }))
 
