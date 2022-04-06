304b710d74bdddacdeaca2b232e59e73a823be03
Your Name <your.email@example.com>
2022-04-06 13:34:46 +0200

    revert to when it was just 401ing overnight sometimes

--- src/frontend/_includes/js/utils/http.js
+++ src/frontend/_includes/js/utils/http.js
@@ -42,14 +42,12 @@ const getErrorStatusCode = (error) => error.response?.status ?? 500
 
 const toAuthHeader = (token) => ({ Authorization: `Bearer ${token}` })
 
-const makeRequest = (method, url, data) => (
-  refreshTokenIfNecessary(),
+const makeRequest = (method, url, data) =>
   NT.ResultAsync.fromPromise(
     axios({ method, url, data, ...tokenIfLoggedIn() })
       .then(({ data }) => data),
     getErrorStatusCode
   )
-)
 
 const tokenIfLoggedIn = () => ({
   headers: Nullable.map(getToken(), toAuthHeader) ?? {}
@@ -64,31 +62,3 @@ const getFirstPathnameSegment = () => {
   const segments = window.location.pathname?.split?.('/').filter(s => s)
   return segments?.[0]
 }
-
-const refreshTokenIfNecessary = () => {
-  console.log("in fn")
-  // If the token in the local storage is newer than the one
-  // on the current tab, then use the one in the local storage
-  // (This happens when you're on multiple memo tabs at once)
-  const localStorageToken = (() => {
-    try {
-      return JSON.parse(localStorage.getItem('gotrue.user')).token
-    } catch {
-      return undefined
-    }
-  })()
-
-  if (localStorageToken?.expires_at > netlifyIdentity.currentUser()?.token?.expires_at) {
-    netlifyIdentity.currentUser().token = localStorageToken
-  }
-
-  if (
-    netlifyIdentity.currentUser()?.token?.expires_at &&
-    netlifyIdentity.currentUser()?.token?.expires_at < Date.now()
-  ) {
-    console.log("Refreshing token")
-    return netlifyIdentity.currentUser()?.jwt?.(true)
-  }
-}
-
-
