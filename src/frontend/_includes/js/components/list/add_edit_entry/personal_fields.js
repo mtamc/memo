440795b4b73f13432ea719d7bdff1a97bab28b89
Tam CARRE <kokone@takat.su>
2022-04-16 12:18:49 +0200

    base `today` on local time instead of UTC

--- src/frontend/_includes/js/components/list/add_edit_entry/personal_fields.js
+++ src/frontend/_includes/js/components/list/add_edit_entry/personal_fields.js
@@ -148,8 +148,15 @@ const CommentsField = (review) => initComponent({
 })
 
 
-const today = () =>
-  (new Date()).toISOString().substring(0, 10)
+const today = () => {
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = String(date.getMonth()+1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
+
 
 const timestampToString = (ts) =>
   (new Date(ts)).toISOString().substring(0, 10)
